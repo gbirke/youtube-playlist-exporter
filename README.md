88878952308f2aa16391a589f3608175ad223c66
Gabriel Birke <gabriel.birke@wikimedia.de>
2023-09-23 22:27:18 +0200

    Add export templates

--- README.md
+++ README.md
@@ -5,11 +5,10 @@ This is a plugin that exports YouTube playlists to text.
 ## TODO / Next steps
 - [x] Render Playlist in popup
 - [x] Show status in popup
-- [ ] 2nd field for render template, with buttons to insert different templates:
+- [x] 2nd field for render template, with buttons to insert different templates:
   - Markdown
   - URLs only
   - CSV
-- [ ] Style popup
 - [ ] Polish manifest
   - [ ] Name and description
   - [ ] Contact information in case of errors
@@ -24,4 +23,5 @@ This is a plugin that exports YouTube playlists to text.
 - [ ] Options UI to configure different templates
 - [ ] Save to file
 - [ ] I18N
+- [ ] Style popup
 - [ ] Allow playlist collection from multiple tabs
--- popup.js
+++ popup.js
@@ -1,8 +1,36 @@
 import van from "./libs/van-1.2.1.min.js"
 
-const { p } = van.tags;
+const { p, select, option, button } = van.tags;
+
+const templates = [
+	{ label: 'URL list', template: "{{url}}\n" },
+	{ label: 'Markdown', template: "- [{{title}}]({{url}})\n" },
+	{ 
+		label: 'CSV',
+		template: "{{title}},{{url}},{{duration}},{{channel}},{{channelLink}}\n",
+		prefix: 'title,url,duration,channel,channelLink\n',
+	},
+];
+
+
+function renderTemplate(template, data) {
+	let text = template;
+	for (const key in data) {
+		text = text.replace(new RegExp(`{{${key}}}`, 'g'), data[key]);
+	}
+	return text;
+}
 
 const appStatus = van.state({ message: '', severity: null });
+const templateIndex = van.state(0);
+
+function onExportButtonClicked() {
+	// TODO add loading indicator
+	// TODO allow collection of playlist data from multiple tabs
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, {type: 'getPlaylistData'}, renderPlaylistResult);
+	});
+}
 
 const Status = () => {
 	const classes = ['status'];
@@ -15,7 +43,19 @@ const Status = () => {
 	return p({class: classes.join(' ')}, appStatus.val.message);
 }
 
+const TemplateSelector = () => {
+	return select({value: templateIndex.val, onchange: e => templateIndex.val = e.target.value},
+		templates.map((template, index) => option({value: index}, template.label))
+	);
+}
+
 van.add(document.getElementById('status'), Status);
+van.add(document.getElementById('export-button'), [
+	'Format: ',
+	TemplateSelector,
+	' ',
+	() => button({onclick: onExportButtonClicked}, 'Export'),
+]);
 
 function setStatusMessage(message, severity) {
 	appStatus.val = { message, severity };
@@ -29,21 +69,15 @@ function renderPlaylistResult(response) {
 		setStatusMessage(response.message, 'error');
 		return;
 	}
+	if (!response.items) {
+		setStatusMessage('Content script returned empty response (no item property)', 'error');
+		return;
+	}
+	setStatusMessage('');
 	var playlistArea = document.getElementById('playlist');
-	let text = ''
-	response.items.forEach(function(item) {
-		text += `- [${item.title}](${item.url})\n`;
-	});
-	playlistArea.value = text;
+	playlistArea.value = response.items.map(item => renderTemplate(templates[templateIndex.val].template, item)).join('');
 }
 
-document.getElementById('export').addEventListener('click', function() {
-	// TODO add loading indicator
-	// TODO allow collection of playlist data from multiple tabs
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, {type: 'getPlaylistData'}, renderPlaylistResult);
-	});
-});
 
 chrome.runtime.onMessage.addListener(function(request) {
 	switch (request.type) {
